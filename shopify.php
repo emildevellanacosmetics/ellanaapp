0470ad76fef9da96fa833538638e389c89ca7835
milski <milski2008@gmail>
2020-03-02 20:43:49 +0800

    test 8

--- shopify.php
+++ shopify.php
@@ -1,8 +1,7 @@
 
 <?php
   //Modify these
-  header("Access-Control-Allow-Origin: *");
-  header("Content-Type:application/json");
+  header("Access-Control-Allow-Origin: *");  
   $API_KEY = '71883cf9cfce84d175fdc94f4e5819f2';
   $SECRET = '58b2593d6f6244ea402434320e31a1dc';
   $TOKEN = '58b2593d6f6244ea402434320e31a1dc';
