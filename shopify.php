5282deb78d93e4374294d720673b3b702a292c5d
milski <milski2008@gmail>
2020-03-02 11:16:43 +0800

    response4

--- shopify.php
+++ shopify.php
@@ -1,6 +1,7 @@
 
 <?php
   //Modify these
+  header("Content-Type:application/json");
   $API_KEY = '71883cf9cfce84d175fdc94f4e5819f2';
   $SECRET = '58b2593d6f6244ea402434320e31a1dc';
   $TOKEN = '58b2593d6f6244ea402434320e31a1dc';
