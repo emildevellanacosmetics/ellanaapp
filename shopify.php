f4d061aeb9cc446c2ace38f873905eaae4ebb0d7
milski <milski2008@gmail>
2020-03-02 19:29:01 +0800

    test 3

--- shopify.php
+++ shopify.php
@@ -28,7 +28,26 @@
       $json_returned = json_decode($response, true);
       echo $json_returned['count'];  
       // echo $product_xml->variants->variant->{'inventory-quantity'};
-   
+   }elseif (isset($_GET['ajaxcall'])=="getshipped"){
+      $url="https://".$API_KEY.":".$SECRET."@".$STORE_URL."/admin/api/2020-01/orders/count.json?fulfillment_status=shipped";
+      $shopcurl = curl_init();
+      curl_setopt($shopcurl, CURLOPT_URL, $url);
+      curl_setopt($shopcurl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+      curl_setopt($shopcurl, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($shopcurl, CURLOPT_VERBOSE, 0);
+      // curl_setopt($shopcurl, CURLOPT_HEADER, 1);
+      curl_setopt($shopcurl, CURLOPT_CUSTOMREQUEST, "GET");
+      curl_setopt($shopcurl, CURLOPT_SSL_VERIFYPEER, false);
+      $response = curl_exec ($shopcurl);
+      curl_close ($shopcurl);
+      
+
+      $json_returned = json_decode($response, true);
+        echo $json_returned['count'];  
+      // echo $product_xml->variants->variant->{'inventory-quantity'};
+  }else{
+    echo "not accepted";
+  }
 
 
 
