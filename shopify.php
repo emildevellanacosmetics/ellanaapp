25c484e2e82a9328b2e7bc8b737847f7af65c0e8
milski <milski2008@gmail>
2020-04-02 16:06:00 +0800

    agdfsf

--- shopify.php
+++ shopify.php
@@ -17,6 +17,13 @@
   require "common.php";
 
 */
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
   if(isset($_POST['beautyquiz'])){
   
  
