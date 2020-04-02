8122879a36e761d379ac9b6cc281ca58ad52e1f4
milski <milski2008@gmail>
2020-04-02 14:15:12 +0800

    dfsdfs2ass

--- shopify.php
+++ shopify.php
@@ -14,14 +14,14 @@
 */
 
   if(isset($_POST['beautyquiz'])){
-   $user = $_POST['beautyquiz']);
+  
  
 //Decode the JSON string and convert it into a PHP associative array.
     
 
 //var_dump the array so that we can view it's structure.
     
-    echo $user;
+    echo "dsfsdf";
       /*  $dataJson = json_decode(stripslashes($_POST['beautyquiz']));
     // here i would like use foreach:  
       $var = $dataJson.iduser ?? "";
