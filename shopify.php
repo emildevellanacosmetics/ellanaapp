33b65b233ed5bacb1342d2d395144bc17a68a6a9
milski <milski2008@gmail>
2020-04-02 14:11:35 +0800

    dfsdfs2as

--- shopify.php
+++ shopify.php
@@ -21,7 +21,7 @@
 
 //var_dump the array so that we can view it's structure.
     
-    echo $user.aq1;
+    echo $user;
       /*  $dataJson = json_decode(stripslashes($_POST['beautyquiz']));
     // here i would like use foreach:  
       $var = $dataJson.iduser ?? "";
