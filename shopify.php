4da78a5560995121f7d56b06a41813c68ecbe06a
milski <milski2008@gmail>
2020-04-09 14:23:19 +0800

    sdfsdsgswf

--- shopify.php
+++ shopify.php
@@ -1,17 +1,17 @@
 <?php
-  //Modify these
-  header("Access-Control-Allow-Origin: *");  
-  $API_KEY = '71883cf9cfce84d175fdc94f4e5819f2';
-  $SECRET = '58b2593d6f6244ea402434320e31a1dc';
-  $TOKEN = '58b2593d6f6244ea402434320e31a1dc';
-  $STORE_URL = 'ellana-cosmetics.myshopify.com';
-  $PRODUCT_ID = 'product-id-here';
+//Modify these
+header("Access-Control-Allow-Origin: *");
+$API_KEY = '71883cf9cfce84d175fdc94f4e5819f2';
+$SECRET = '58b2593d6f6244ea402434320e31a1dc';
+$TOKEN = '58b2593d6f6244ea402434320e31a1dc';
+$STORE_URL = 'ellana-cosmetics.myshopify.com';
+$PRODUCT_ID = 'product-id-here';
 
-  $servername = "us-cdbr-iron-east-04.cleardb.net";
-  $username = "b373f528b8e38d";
-  $password = "e6cdc6ea";
-  $dbname = "heroku_30c47afc2d3c720";
-  
+$servername = "us-cdbr-iron-east-04.cleardb.net";
+$username = "b373f528b8e38d";
+$password = "e6cdc6ea";
+$dbname = "heroku_30c47afc2d3c720";
+
 /*
   require "config.php";
   require "common.php";
@@ -20,124 +20,133 @@
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
-if ($conn->connect_error) {
+if ($conn->connect_error)
+{
     die("Connection failed: " . $conn->connect_error);
 }
 
-  if(isset($_POST['beautyquiz'])){
-  
- 
-//var_dump the array so that we can view it's structure.
-    
-   // $data = json_encode($_POST['beautyquiz']);
-  $data = $_POST['beautyquiz'];
-      
-       $aq1 = $data['aq1'];
-       $aq2 = $data['aq2'];
-       $aq3 = $data['aq3'];
-       $bq1 = $data['bq1'];
-       $bq2 = $data['bq2'];
-       $bq3 = $data['bq3'];
-       $cq1 = $data['cq1'];
-       $dq1 = $data['dq1'];
-       $iduser = $data['iduser'];
-      
-        $sql = "INSERT INTO user                
-                (
-                iduser,
-                aq1,
-                aq2,
-                aq3,
-                bq1,
-                bq2,
-                bq3,
-                cq1,
-                dq1
-                ) VALUES (
-                 '".$iduser."',
-                '".$aq1."',
-                '".$aq2."',
-                '".$aq3."',
-                '".$bq1."',
-                '".$bq2."',
-                '".$bq3."',
-                '".$cq1."',
-                '".$dq1."'            
-                )";
+if (isset($_POST['beautyquiz']))
+{
 
-                
+    //var_dump the array so that we can view it's structure.
+    // $data = json_encode($_POST['beautyquiz']);
+    $data = $_POST['beautyquiz'];
 
-                
-                if ($conn->query($sql) === TRUE) {
-                    echo "New record created successfully";
-                } else {
-                }
-                
+    $aq1 = $data['aq1'];
+    $aq2 = $data['aq2'];
+    $aq3 = $data['aq3'];
+    $bq1 = $data['bq1'];
+    $bq2 = $data['bq2'];
+    $bq3 = $data['bq3'];
+    $cq1 = $data['cq1'];
+    $dq1 = $data['dq1'];
+    $iduser = $data['iduser'];
+
+    $query = "SELECT * from user where iduser='" . $iduser . "'";
+    $result = mysql_query($query);
+
+    if (mysql_num_rows($result) > 0)
+    {
+        $sql = "UPDATE user
+        SET
+            aq1 = '" . $aq1 . "',
+            aq2 = '" . $aq2 . "',
+            aq3 = '" . $aq3 . "',
+            bq1 = '" . $bq1 . "',
+            bq2 = '" . $bq2 . "',
+            bq3 = '" . $bq3 . "',
+            cq1 = '" . $cq1 . "',
+            dq1 = '" . $dq1 . "'
+        WHERE iduser = '" . $iduser . "'; 
+
+        if ($conn->query($sql) === TRUE) {
+          echo "Recordupdatedsuccessfully";
+        } else {
+          echo "UpdateError";
+        }
+       }else{
+     
+
+       }
+       
+
                 $conn->close();              
 
 
   }else{
-    echo "not recieved 2";
+    echo "notrecieved2";
   }
 
 
 
   if(isset($_GET['ajaxcall'])){
   if ($_GET['ajaxcall']=='getall') {
-      $url="https://".$API_KEY.":".$SECRET."@".$STORE_URL."/admin/api/2020-01/orders/count.json?status=any";
-      $shopcurl = curl_init();
-      curl_setopt($shopcurl, CURLOPT_URL, $url);
-      curl_setopt($shopcurl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-      curl_setopt($shopcurl, CURLOPT_RETURNTRANSFER, true);
-      curl_setopt($shopcurl, CURLOPT_VERBOSE, 0);
-      // curl_setopt($shopcurl, CURLOPT_HEADER, 1);
-      curl_setopt($shopcurl, CURLOPT_CUSTOMREQUEST, "GET");
-      curl_setopt($shopcurl, CURLOPT_SSL_VERIFYPEER, false);
-      $response = curl_exec ($shopcurl);
-      curl_close ($shopcurl);      
-      $json_returned = json_decode($response, true);
-      echo $json_returned['count'];  
-      // echo $product_xml->variants->variant->{'inventory-quantity'};
-   }elseif ($_GET['ajaxcall']=='getshipped'){
-      $url="https://".$API_KEY.":".$SECRET."@".$STORE_URL."/admin/api/2020-01/orders/count.json?fulfillment_status=shipped";
-      $shopcurl = curl_init();
-      curl_setopt($shopcurl, CURLOPT_URL, $url);
-      curl_setopt($shopcurl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-      curl_setopt($shopcurl, CURLOPT_RETURNTRANSFER, true);
-      curl_setopt($shopcurl, CURLOPT_VERBOSE, 0);
-      // curl_setopt($shopcurl, CURLOPT_HEADER, 1);
-      curl_setopt($shopcurl, CURLOPT_CUSTOMREQUEST, "GET");
-      curl_setopt($shopcurl, CURLOPT_SSL_VERIFYPEER, false);
-      $response = curl_exec ($shopcurl);
-      curl_close ($shopcurl);
-      
+      $url="https: //".$API_KEY.":".$SECRET."@".$STORE_URL."/admin/api/2020-01/orders/count.json?status=any";
+            $shopcurl = curl_init();
+            curl_setopt($shopcurl, CURLOPT_URL, $url);
+            curl_setopt($shopcurl, CURLOPT_HTTPHEADER, array(
+                'Content-Type: application/json'
+            ));
+            curl_setopt($shopcurl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($shopcurl, CURLOPT_VERBOSE, 0);
+            // curl_setopt($shopcurl, CURLOPT_HEADER, 1);
+            curl_setopt($shopcurl, CURLOPT_CUSTOMREQUEST, "GET");
+            curl_setopt($shopcurl, CURLOPT_SSL_VERIFYPEER, false);
+            $response = curl_exec($shopcurl);
+            curl_close($shopcurl);
+            $json_returned = json_decode($response, true);
+            echo $json_returned['count'];
+            // echo $product_xml->variants->variant->{'inventory-quantity'};
+            
+        }
+        elseif ($_GET['ajaxcall'] == 'getshipped')
+        {
+            $url = "https://" . $API_KEY . ":" . $SECRET . "@" . $STORE_URL . "/admin/api/2020-01/orders/count.json?fulfillment_status=shipped";
+            $shopcurl = curl_init();
+            curl_setopt($shopcurl, CURLOPT_URL, $url);
+            curl_setopt($shopcurl, CURLOPT_HTTPHEADER, array(
+                'Content-Type: application/json'
+            ));
+            curl_setopt($shopcurl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($shopcurl, CURLOPT_VERBOSE, 0);
+            // curl_setopt($shopcurl, CURLOPT_HEADER, 1);
+            curl_setopt($shopcurl, CURLOPT_CUSTOMREQUEST, "GET");
+            curl_setopt($shopcurl, CURLOPT_SSL_VERIFYPEER, false);
+            $response = curl_exec($shopcurl);
+            curl_close($shopcurl);
 
-      $json_returned = json_decode($response, true);
-        echo $json_returned['count'];  
-      // echo $product_xml->variants->variant->{'inventory-quantity'};
-  }elseif ($_GET['ajaxcall']=='customers') {
-    
-    $url="https://".$API_KEY.":".$SECRET."@".$STORE_URL."/admin/api/2020-01/customers/count.json";
-    $shopcurl = curl_init();
-    curl_setopt($shopcurl, CURLOPT_URL, $url);
-    curl_setopt($shopcurl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-    curl_setopt($shopcurl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($shopcurl, CURLOPT_VERBOSE, 0);
-    // curl_setopt($shopcurl, CURLOPT_HEADER, 1);
-    curl_setopt($shopcurl, CURLOPT_CUSTOMREQUEST, "GET");
-    curl_setopt($shopcurl, CURLOPT_SSL_VERIFYPEER, false);
-    $response = curl_exec ($shopcurl);
-    curl_close ($shopcurl);
-    
+            $json_returned = json_decode($response, true);
+            echo $json_returned['count'];
+            // echo $product_xml->variants->variant->{'inventory-quantity'};
+            
+        }
+        elseif ($_GET['ajaxcall'] == 'customers')
+        {
 
-    $json_returned = json_decode($response, true);
-      echo $json_returned['count'];  
-    // echo $product_xml->variants->variant->{'inventory-quantity'};
-  }else{
-    echo "not accepted";
-  }
+            $url = "https://" . $API_KEY . ":" . $SECRET . "@" . $STORE_URL . "/admin/api/2020-01/customers/count.json";
+            $shopcurl = curl_init();
+            curl_setopt($shopcurl, CURLOPT_URL, $url);
+            curl_setopt($shopcurl, CURLOPT_HTTPHEADER, array(
+                'Content-Type: application/json'
+            ));
+            curl_setopt($shopcurl, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($shopcurl, CURLOPT_VERBOSE, 0);
+            // curl_setopt($shopcurl, CURLOPT_HEADER, 1);
+            curl_setopt($shopcurl, CURLOPT_CUSTOMREQUEST, "GET");
+            curl_setopt($shopcurl, CURLOPT_SSL_VERIFYPEER, false);
+            $response = curl_exec($shopcurl);
+            curl_close($shopcurl);
 
-  }
+            $json_returned = json_decode($response, true);
+            echo $json_returned['count'];
+            // echo $product_xml->variants->variant->{'inventory-quantity'};
+            
+        }
+        else
+        {
+            echo "not accepted";
+        }
 
+    }
 
 ?>
