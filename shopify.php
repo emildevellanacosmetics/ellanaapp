1f0a007f2a757e43e740392c701eb4ca07c05fa7
milski <milski2008@gmail>
2020-04-12 21:25:00 +0800

    hdfgsdfg

--- shopify.php
+++ shopify.php
@@ -1,6 +1,6 @@
 <?php
 //Modify these
-// header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: *");
 header("content-type: application/javascript");
 $API_KEY = '71883cf9cfce84d175fdc94f4e5819f2';
 $SECRET = '58b2593d6f6244ea402434320e31a1dc';
