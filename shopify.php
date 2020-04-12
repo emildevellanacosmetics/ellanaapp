da2c92b2b7102b9830afd96800d4cca99a4d1b3b
milski <milski2008@gmail>
2020-04-13 00:59:44 +0800

    ggdfdfs

--- shopify.php
+++ shopify.php
@@ -9,14 +9,18 @@ $TOKEN = '58b2593d6f6244ea402434320e31a1dc';
 $STORE_URL = 'ellana-cosmetics.myshopify.com';
 $PRODUCT_ID = 'product-id-here';
 
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$servername = "us-cdbr-iron-east-04.cleardb.net";
+$username = "b373f528b8e38d";
+$password = "e6cdc6ea";
+$dbname = "heroku_30c47afc2d3c720";
 
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
+/*
+  require "config.php";
+  require "common.php";
 
-$conn = new mysqli($server, $username, $password, $db);
+*/
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error)
 {
