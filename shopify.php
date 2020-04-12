b7e7faf20fe8860b4deedff6af103432b3ddfcd5
milski <milski2008@gmail>
2020-04-13 02:48:37 +0800

    gsgsdf

--- shopify.php
+++ shopify.php
@@ -49,24 +49,8 @@ if (isset($_POST['beautyquiz']))
     $dq1 = $data['dq1']; 
     $iduser = $data['iduser'];
  
-    $sql = "UPDATE user
-    SET
-    aq1 = '" . $aq1 . "',
-    aq2 = '" . $aq2 . "',
-    aq3 = '" . $aq3 . "',
-    bq1 = '" . $bq1 . "',
-    bq2 = '" . $bq2 . "',
-    bq3 = '" . $bq3 . "',
-    cq1 = '" . $cq1 . "',
-    dq1 = '" . $dq1 . "'
-WHERE iduser = '" . $iduser . "';
-
-if ($conn->query($sql) === TRUE) {
-  echo "Recordupdatedsuccessfully";
-} else {
-  echo "Update Error";
-}
-
+  echo "sdfsdfsdf";
+  
   $conn->close();              
 
   }
